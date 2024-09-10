3dfc1979c5ba6438cfdcd2ab2a0dec9428d786a0
JacksonHoggard <jhoggard0129@gmail.com>
2024-09-10 15:57:38 -0500

    Update dependencies

---
+++ src/main/java/me/jacksonhoggard/raydream/gui/editor/model/OBJModel.java
@@ -0,0 +1,18 @@
+package me.jacksonhoggard.raydream.gui.editor.model;
+
+public class OBJModel extends EditorModel {
+
+    public OBJModel() {
+        super();
+    }
+
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void remove() {
+
+    }
+}
